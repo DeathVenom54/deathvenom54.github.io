5d04c097a82276a7e2531b8416aa8e242d26b7d4
DeathVenom54 <fdragon602@gmail.com>
2022-03-27 11:11:20 +0530

    temp push fixing vite errors

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "./router";
+import router from "./router/index.js";
 
 const Vue = createApp(App).use(router);
 Vue.mount("#app");
